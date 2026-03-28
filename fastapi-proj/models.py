c5c331b2b3cfc54951b32b83d718bd92707f4bef
Thiago Marçal <thiagomarcal1984@gmail.com>
2026-03-28 11:55:52 -0300

    Criando o models.py

---
+++ fastapi-proj/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+)
+from database import Base
+
+class Estudante(Base):
+    __tablename__ = 'estudantes'
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+    nome = Column(
+        String(100),
+        nullable=False,
+    )
+    age = Column(Integer)
+
+class Matricula(Base):
+    __tablename__ = 'matriculas'
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+    estudante_id = Column(
+        Integer,
+        ForeignKey('estudante.id'),
+    )
+    nome_disciplina = Column(
+        String(100),
+        nullable=False,
+    )
